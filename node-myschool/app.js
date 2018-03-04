4444485b6f9d74f789c0a79ec57ccc62edf5a278
lintao <123@qq.com>
2018-03-04 13:55:26 +0800

    2018-3-4
    笔记更新

---
+++ node-myschool/app.js
@@ -0,0 +1,52 @@
+const request = require('superagent')
+const cheerio = require('cheerio')
+const fs = require('fs-extra')
+const path = require('path')
+
+
+
+let url = 'http://www.mmjpg.com'
+// sleep函数
+function sleep(time) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve()
+        }, time)
+    })
+  };
+  
+function random(min,max){
+    let range = max - min
+    let rand = Math.random()
+    let num = min + Math.round(rand * range)
+    return num
+};
+async function getUrl() {
+    let linkArr = []
+    const res = await request.get(url)
+    const $ = cheerio.load(res.text)
+    $('.pic ul li').each(function (i, elem) {
+    let link = $(this).find('a img').attr('src')
+    linkArr.push(link)
+    })
+    console.log(linkArr)
+    return linkArr
+  }
+
+function download(imgUrl){
+    console.log(`正在下载${imgUrl}`)
+    const filename = imgUrl.split('/').pop()  
+    const req = request.get(imgUrl).set({ 'Referer': 'http://www.mmjpg.com' })
+    req.pipe(fs.createWriteStream(path.join(__dirname, 'mm', filename)))
+  }
+
+  async function init(){
+    let urls = await getUrl();
+    for(let i = 0; i < urls.length; i++ )
+    {
+        download(urls[i])
+        await sleep(random(1000, 5000)) 
+    }
+  }
+
+  init()
